5852d297d08962f73bb802ac0c59becd623e8f5e
Mike Klophaus <mmklophaus@gmail.com>
2016-02-08 21:22:53 -0800

    added tasks routes

--- config/routes.js
+++ config/routes.js
@@ -4,12 +4,20 @@ var express = require('express'),
 // Require controllers.
 var welcomeController = require('../controllers/welcome');
 var usersController   = require('../controllers/users');
-
+var tasksController   = require('../controllers/tasks');
 // root path:
 router.get('/', welcomeController.index);
 
 // users resource paths:
 router.get('/users',     usersController.index);
 router.get('/users/:id', usersController.show);
+
+
+///DOCS ROUTES
+router.get('/tasks/:id',    tasksController.taskShow);
+router.get('/tasks',       tasksController.taskIndex);
+router.post('/tasks',      tasksController.taskCreate);
+
+
 
 module.exports = router;
